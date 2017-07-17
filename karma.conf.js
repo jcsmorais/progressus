ea69c26123a7ea0966709efd7d4b7552c4bd0c81
jcsmorais <jcsmorais@gmail.com>
2017-07-16 23:54:02 -0700

    Add karma.conf.js and setup npm test script

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+let istanbul = require('browserify-istanbul');
+
+module.exports = function (config) {
+    config.set({
+        frameworks: [
+            'browserify',
+            'mocha',
+            'source-map-support',
+        ],
+        files: [
+            'src/**/*.js',
+            'test/**/*.js',
+        ],
+        reporters: [
+            'spec',
+            'coverage',
+        ],
+        preprocessors: {
+            'src/**/*.js': ['browserify'],
+            'test/**/*.js': ['browserify'],
+        },
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ['ChromeHeadless'],
+        autoWatch: false,
+        singleRun: true,
+        browserify: {
+            debug: true,
+            transform: [istanbul]
+        },
+        coverageReporter: {
+            reporters: [
+                { type: 'text-summary' },
+                { type: 'html' },
+            ],
+        }
+    })
+}
